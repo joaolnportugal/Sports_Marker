cede06ca1261597536eee0c75ea66a2461c795ee
João Portugal <joaoportugal@blissapplications.com>
2022-08-10 15:49:14 +0100

    first commit

---
+++ Sports_Marker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sports_Marker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
